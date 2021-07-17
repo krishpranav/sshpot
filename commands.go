c5ae86393bc9df9339b55e4a7e8730b3196c2e9e
krishpranav <krisna.pranav@gmail.com>
2021-07-17 18:58:24 +0530

    commands: executeProgram, cmdshell struct

--- commands.go
+++ commands.go
@@ -31,3 +31,17 @@ var commands = map[string]command{
 }
 
 var shellProgram = []string{"sh"}
+
+func executeProgram(context commandContext) (uint32, error) {
+	if len(context.args) == 0 {
+		return 0, nil
+	}
+	command := commands[context.args[0]]
+	if command == nil {
+		_, err := fmt.Fprintf(context.stderr, "%v: command not found\n", context.args[0])
+		return 127, err
+	}
+	return command.execute(context)
+}
+
+type cmdShell struct{}
