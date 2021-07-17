73810c18f00fcb887c77f737db53abad1bb86c2d
krishpranav <krisna.pranav@gmail.com>
2021-07-17 18:10:18 +0530

    auth_test: test classes

--- auth_test.go
+++ auth_test.go
@@ -32,3 +32,30 @@ func (context mockConnContext) RemoteAddr() net.Addr {
 func (context mockConnContext) LocalAddr() net.Addr {
 	return &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 2022}
 }
+
+func TestAuthLogUninteresting(t *testing.T) {
+	cfg := &config{}
+	cfg.Auth.NoAuth = false
+	callback := cfg.getAuthLogCallback()
+	logBuffer := setupLogBuffer(t, cfg)
+	callback(mockConnContext{}, "password", nil)
+	logs := logBuffer.String()
+	expectedLogs := ``
+	if logs != expectedLogs {
+		t.Errorf("logs=%v, want %v", string(logs), expectedLogs)
+	}
+}
+
+func TestNoAuthFail(t *testing.T) {
+	cfg := &config{}
+	cfg.Auth.NoAuth = false
+	callback := cfg.getAuthLogCallback()
+	logBuffer := setupLogBuffer(t, cfg)
+	callback(mockConnContext{}, "none", errors.New(""))
+	logs := logBuffer.String()
+	expectedLogs := `[127.0.0.1:1234] authentication for user "root" without credentials rejected
+`
+	if logs != expectedLogs {
+		t.Errorf("logs=%v, want %v", string(logs), expectedLogs)
+	}
+}
