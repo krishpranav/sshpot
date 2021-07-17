08a4763771fc6b1af24b0595cd20c29732f47f62
krishpranav <krisna.pranav@gmail.com>
2021-07-17 18:45:50 +0530

    config_test: verify config file

--- config_test.go
+++ config_test.go
@@ -42,3 +42,71 @@ func (file *mocFile) Close() error {
 	file.closed = true
 	return nil
 }
+
+func verifyConfig(t *testing.T, cfg *config, expected *config) {
+	if !reflect.DeepEqual(cfg.Server, expected.Server) {
+		t.Errorf("Server=%v, want %v", cfg.Server, expected.Server)
+	}
+	if !reflect.DeepEqual(cfg.Logging, expected.Logging) {
+		t.Errorf("Logging=%v, want %v", cfg.Logging, expected.Logging)
+	}
+	if !reflect.DeepEqual(cfg.Auth, expected.Auth) {
+		t.Errorf("Auth=%v, want %v", cfg.Auth, expected.Auth)
+	}
+	if !reflect.DeepEqual(cfg.SSHProto, expected.SSHProto) {
+		t.Errorf("SSHProto=%v, want %v", cfg.SSHProto, expected.SSHProto)
+	}
+
+	if cfg.sshConfig.RekeyThreshold != expected.SSHProto.RekeyThreshold {
+		t.Errorf("sshConfig.RekeyThreshold=%v, want %v", cfg.sshConfig.RekeyThreshold, expected.SSHProto.RekeyThreshold)
+	}
+	if !reflect.DeepEqual(cfg.sshConfig.KeyExchanges, expected.SSHProto.KeyExchanges) {
+		t.Errorf("sshConfig.KeyExchanges=%v, want %v", cfg.sshConfig.KeyExchanges, expected.SSHProto.KeyExchanges)
+	}
+	if !reflect.DeepEqual(cfg.sshConfig.Ciphers, expected.SSHProto.Ciphers) {
+		t.Errorf("sshConfig.Ciphers=%v, want %v", cfg.sshConfig.Ciphers, expected.SSHProto.Ciphers)
+	}
+	if !reflect.DeepEqual(cfg.sshConfig.MACs, expected.SSHProto.MACs) {
+		t.Errorf("sshConfig.MACs=%v, want %v", cfg.sshConfig.MACs, expected.SSHProto.MACs)
+	}
+	if cfg.sshConfig.NoClientAuth != expected.Auth.NoAuth {
+		t.Errorf("sshConfig.NoClientAuth=%v, want %v", cfg.sshConfig.NoClientAuth, expected.Auth.NoAuth)
+	}
+	if cfg.sshConfig.MaxAuthTries != expected.Auth.MaxTries {
+		t.Errorf("sshConfig.MaxAuthTries=%v, want %v", cfg.sshConfig.MaxAuthTries, expected.Auth.MaxTries)
+	}
+	if (cfg.sshConfig.PasswordCallback != nil) != expected.Auth.PasswordAuth.Enabled {
+		t.Errorf("sshConfig.PasswordCallback=%v, want %v", cfg.sshConfig.PasswordCallback != nil, expected.Auth.PasswordAuth.Enabled)
+	}
+	if (cfg.sshConfig.PublicKeyCallback != nil) != expected.Auth.PublicKeyAuth.Enabled {
+		t.Errorf("sshConfig.PasswordCallback=%v, want %v", cfg.sshConfig.PublicKeyCallback != nil, expected.Auth.PublicKeyAuth.Enabled)
+	}
+	if (cfg.sshConfig.KeyboardInteractiveCallback != nil) != expected.Auth.KeyboardInteractiveAuth.Enabled {
+		t.Errorf("sshConfig.KeyboardInteractiveCallback=%v, want %v", cfg.sshConfig.KeyboardInteractiveCallback != nil, expected.Auth.KeyboardInteractiveAuth.Enabled)
+	}
+	if cfg.sshConfig.AuthLogCallback == nil {
+		t.Errorf("sshConfig.AuthLogCallback=nil, want a callback")
+	}
+	if cfg.sshConfig.ServerVersion != expected.SSHProto.Version {
+		t.Errorf("sshConfig.ServerVersion=%v, want %v", cfg.sshConfig.ServerVersion, expected.SSHProto.Version)
+	}
+	if (cfg.sshConfig.BannerCallback != nil) != (expected.SSHProto.Banner != "") {
+		t.Errorf("sshConfig.BannerCallback=%v, want %v", cfg.sshConfig.BannerCallback != nil, expected.SSHProto.Banner != "")
+	}
+	if cfg.sshConfig.GSSAPIWithMICConfig != nil {
+		t.Errorf("sshConfig.GSSAPIWithMICConfig=%v, want nil", cfg.sshConfig.GSSAPIWithMICConfig)
+	}
+	if len(cfg.parsedHostKeys) != len(expected.Server.HostKeys) {
+		t.Errorf("len(parsedHostKeys)=%v, want %v", len(cfg.parsedHostKeys), len(expected.Server.HostKeys))
+	}
+
+	if expected.Logging.File == "" {
+		if cfg.logFileHandle != nil {
+			t.Errorf("logFileHandle=%v, want nil", cfg.logFileHandle)
+		}
+	} else {
+		if cfg.logFileHandle == nil {
+			t.Errorf("logFileHandle=nil, want a file")
+		}
+	}
+}
