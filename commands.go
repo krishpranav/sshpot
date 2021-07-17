90d6319958ca0dd1f6fed1d66688e3d8fcf94564
krishpranav <krisna.pranav@gmail.com>
2021-07-17 18:58:54 +0530

    commands: execute

--- commands.go
+++ commands.go
@@ -45,3 +45,39 @@ func executeProgram(context commandContext) (uint32, error) {
 }
 
 type cmdShell struct{}
+
+func (cmdShell) execute(context commandContext) (uint32, error) {
+	var prompt string
+	if context.pty {
+		prompt = "$ "
+	}
+	var line string
+	var err error
+	for {
+		_, err = fmt.Fprint(context.stdout, prompt)
+		if err != nil {
+			return 0, err
+		}
+		line, err = context.stdin.ReadLine()
+		if err != nil {
+			return 0, err
+		}
+		args := strings.Fields(line)
+		if len(args) > 0 && args[0] == "exit" {
+			var err error
+			var status uint64
+			if len(args) > 1 {
+				status, err = strconv.ParseUint(args[1], 10, 32)
+				if err != nil {
+					status = 255
+				}
+			}
+			return uint32(status), nil
+		}
+		newContext := context
+		newContext.args = strings.Fields(line)
+		if _, err = executeProgram(newContext); err != nil {
+			return 0, err
+		}
+	}
+}
