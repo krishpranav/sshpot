f26f70e4e571fb7ceae267d18d79d1055fecb4c2
krishpranav <krisna.pranav@gmail.com>
2021-07-17 19:38:28 +0530

    session: request window

--- session.go
+++ session.go
@@ -48,3 +48,81 @@ type x11RequestPayload struct {
 	AuthProtocol, AuthCookie string
 	ScreenNumber             uint32
 }
+
+func (request x11RequestPayload) reply() []byte {
+	return nil
+}
+func (request x11RequestPayload) logEntry(channelID int) logEntry {
+	return x11Log{
+		channelLog: channelLog{
+			ChannelID: channelID,
+		},
+		Screen: request.ScreenNumber,
+	}
+}
+
+type envRequestPayload struct {
+	Name, Value string
+}
+
+func (request envRequestPayload) reply() []byte {
+	return nil
+}
+func (request envRequestPayload) logEntry(channelID int) logEntry {
+	return envLog{
+		channelLog: channelLog{
+			ChannelID: channelID,
+		},
+		Name:  request.Name,
+		Value: request.Value,
+	}
+}
+
+type execRequestPayload struct {
+	Command string
+}
+
+func (request execRequestPayload) reply() []byte {
+	return nil
+}
+func (request execRequestPayload) logEntry(channelID int) logEntry {
+	return execLog{
+		channelLog: channelLog{
+			ChannelID: channelID,
+		},
+		Command: request.Command,
+	}
+}
+
+type subsystemRequestPayload struct {
+	Subsystem string
+}
+
+func (request subsystemRequestPayload) reply() []byte {
+	return nil
+}
+func (request subsystemRequestPayload) logEntry(channelID int) logEntry {
+	return subsystemLog{
+		channelLog: channelLog{
+			ChannelID: channelID,
+		},
+		Subsystem: request.Subsystem,
+	}
+}
+
+type windowChangeRequestPayload struct {
+	Width, Height, PixelWidth, PixelHeight uint32
+}
+
+func (request windowChangeRequestPayload) reply() []byte {
+	return nil
+}
+func (request windowChangeRequestPayload) logEntry(channelID int) logEntry {
+	return windowChangeLog{
+		channelLog: channelLog{
+			ChannelID: channelID,
+		},
+		Width:  request.Width,
+		Height: request.Height,
+	}
+}
