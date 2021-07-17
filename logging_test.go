1db7e4ad49e9580924a343d1daaa978990f2c68c
krishpranav <krisna.pranav@gmail.com>
2021-07-17 18:16:35 +0530

    logging_test functions

--- logging_test.go
+++ logging_test.go
@@ -51,3 +51,37 @@ $`)
 		t.Errorf("logs=%v, want match for %v", logs, expectedLogs)
 	}
 }
+
+func TestPlainWithoutTimestamps(t *testing.T) {
+	cfg := &config{
+		Logging: loggingConfig{
+			JSON:       false,
+			Timestamps: false,
+		},
+	}
+	logBuffer := setupLogBuffer(t, cfg)
+	connContext{ConnMetadata: mockConnContext{}, cfg: cfg}.logEvent(mockLogEntry{"dolor"})
+	logs := logBuffer.String()
+	expectedLogs := `[127.0.0.1:1234] test dolor
+`
+	if logs != expectedLogs {
+		t.Errorf("logs=%v, want %v", logs, expectedLogs)
+	}
+}
+
+func TestJSONWithoutTimestamps(t *testing.T) {
+	cfg := &config{
+		Logging: loggingConfig{
+			JSON:       true,
+			Timestamps: false,
+		},
+	}
+	logBuffer := setupLogBuffer(t, cfg)
+	connContext{ConnMetadata: mockConnContext{}, cfg: cfg}.logEvent(mockLogEntry{"sit"})
+	logs := logBuffer.String()
+	expectedLogs := `{"source":"127.0.0.1:1234","event_type":"test","event":{"content":"sit"}}
+`
+	if logs != expectedLogs {
+		t.Errorf("logs=%v, want %v", logs, expectedLogs)
+	}
+}
