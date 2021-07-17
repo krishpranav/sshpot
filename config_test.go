ecd6b1ceeeccab8b9b942c9f25e32f8ad6f90244
krishpranav <krisna.pranav@gmail.com>
2021-07-17 18:44:54 +0530

    config_test: functions, typevalues

---
+++ config_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path"
+	"reflect"
+	"testing"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type mockPublicKey struct {
+	signature keySignature
+}
+
+func (publicKey mockPublicKey) Type() string {
+	return publicKey.signature.String()
+}
+
+func (publicKey mockPublicKey) Marshal() []byte {
+	return []byte(publicKey.signature.String())
+}
+
+func (publicKey mockPublicKey) Verify(data []byte, sig *ssh.Signature) error {
+	return nil
+}
+
+type mockFile struct {
+	closed bool
+}
+
+func (file *mockFile) Write(p []byte) (n int, err error) {
+	return 0, errors.New("")
+}
