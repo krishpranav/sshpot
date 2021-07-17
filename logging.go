57f395ab72b9f9cce9d0d5ab37999f8daa5d2e3a
krishpranav <krisna.pranav@gmail.com>
2021-07-17 19:24:03 +0530

    logging: type, funcs

--- logging.go
+++ logging.go
@@ -21,3 +21,19 @@ func (accepted authAccepted) String() string {
 	}
 	return "rejected"
 }
+
+type authLog struct {
+	User     string       `json:"user"`
+	Accepted authAccepted `json:"accepted"`
+}
+
+type noAuthLog struct {
+	authLog
+}
+
+func (entry noAuthLog) String() string {
+	return fmt.Sprintf("authentication for user %q without credentials %v", entry.User, entry.Accepted)
+}
+func (entry noAuthLog) eventType() string {
+	return "no_auth"
+}
