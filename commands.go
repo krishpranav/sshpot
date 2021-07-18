2b36be41bfdcc334db58e47b5aecae7c4f4270d9
krishpranav <krisna.pranav@gmail.com>
2021-07-18 10:06:33 +0530

    commands

--- commands.go
+++ commands.go
@@ -81,3 +81,44 @@ func (cmdShell) execute(context commandContext) (uint32, error) {
 		}
 	}
 }
+
+type cmdTrue struct{}
+
+func (cmdTrue) execute(context commandContext) (uint32, error) {
+	return 0, nil
+}
+
+type cmdFalse struct{}
+
+func (cmdFalse) execute(context commandContext) (uint32, error) {
+	return 1, nil
+}
+
+type cmdEcho struct{}
+
+func (cmdEcho) execute(context commandContext) (uint32, error) {
+	_, err := fmt.Fprintln(context.stdout, strings.Join(context.args[1:], " "))
+	return 0, err
+}
+
+type cmdCat struct{}
+
+func (cmdCat) execute(context commandContext) (uint32, error) {
+	if len(context.args) > 1 {
+		for _, file := range context.args[1:] {
+			if _, err := fmt.Fprintf(context.stderr, "%v: %v: No such file or directory\n", context.args[0], file); err != nil {
+				return 0, err
+			}
+		}
+		return 1, nil
+	}
+	var line string
+	var err error
+	for err == nil {
+		line, err = context.stdin.ReadLine()
+		if err == nil {
+			_, err = fmt.Fprintln(context.stdout, line)
+		}
+	}
+	return 0, err
+}
